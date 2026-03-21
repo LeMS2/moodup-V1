23c1a2e182ecbf46cb17e78bdd537aff5f99ba4c
LeMS2 <leticia-soares26@hotmail.com>
2026-03-21 16:13:49 -0300

    fix: adicione config de cors para web local

---
+++ config/config.php
@@ -0,0 +1,19 @@
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://localhost:8081',
+        'https://127.0.0.1.8081',
+    ],
+
+    'allowed_origins_patters' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];

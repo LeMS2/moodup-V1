db9e252e94e80369f0227801d00abac59120eb13
Lilith774 <lyllyan60@gmail.com>
2026-06-11 18:52:04 -0300

    Tentativa de titulo

--- app/Http/Resources/MoodResource.php
+++ app/Http/Resources/MoodResource.php
@@ -1,28 +1,38 @@
-public function toArray(Request $request): array
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MoodResource extends JsonResource
 {
-    return [
-        'id' => $this->id,
-        'title' => $this->title,
-        'date' => $this->date,
-        'level' => $this->level,
-        'score' => $this->score,
-        'mood' => $this->mood,
-        'note' => $this->note,
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'date' => $this->date,
+            'level' => $this->level,
+            'score' => $this->score,
+            'mood' => $this->mood,
+            'note' => $this->note,
 
-        'categories' => CategoryResource::collection(
-            $this->whenLoaded('categories')
-        ),
+            'categories' => CategoryResource::collection(
+                $this->whenLoaded('categories')
+            ),
 
-        'triggers' => $this->whenLoaded('triggers', function () {
-            return $this->triggers->map(function ($trigger) {
-                return [
-                    'id' => $trigger->id,
-                    'name' => $trigger->name,
-                ];
-            });
-        }),
+            'triggers' => $this->whenLoaded('triggers', function () {
+                return $this->triggers->map(function ($trigger) {
+                    return [
+                        'id' => $trigger->id,
+                        'name' => $trigger->name,
+                    ];
+                });
+            }),
 
-        'created_at' => $this->created_at,
-        'updated_at' => $this->updated_at,
-    ];
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
 }
